d94198949839a246fd589320d0fd030710d4055e
James David R. Maserin <maserinjamesdavid500@gmail.com>
2025-04-19 14:38:58 +0800

    Version 1.0: Complete

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,7 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://ejgelmxicmlgzzpksbip.supabase.co';
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqZ2VsbXhpY21sZ3p6cGtzYmlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNTYyOTIsImV4cCI6MjA1OTkzMjI5Mn0.NwFbBXC1WUWy4E75ZevoMsSPCojJG8CFnGxhkb7zzmA';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
